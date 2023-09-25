89617f3695e5519bede6731decd2b6e9295851df
Shweta <doaguruinfosystems@gmail.com>
2023-09-25 11:57:30 +0530

    tailwind update

---
+++ src/Example.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Example() {
+  return (
+   <>
+   
+   <h1 className='text-yellow-400 font-mono '>Hello Tailwind</h1>
+   
+   
+   </>
+  )
+}
+
+export default Example
---
+++ src/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div className='bg-blue-50'>
+      <p className="">Hello, Tailwind CSS!</p>
+      <div class="shadow bg-white rounded-lg">
+    shadow
+  </div>
+    </div>
+  );
+}
+
+export default MyComponent;
